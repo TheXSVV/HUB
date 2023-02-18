17a7f43377aa53944091073d76fd5cacb5623158
TheXSVV <sowgsgd@gmail.com>
2023-02-18 23:39:03 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,9 @@
 function loadScript(script)
-    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/TheXSVV/HUB/main/' .. script .. '.lua'))()
+    return loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/TheXSVV/HUB/main/' .. script .. '.lua'))()
 end
 
 local checkbox = loadScript('Checkbox')
 local feature = loadScript('Feature')
 local window = loadScript('Window')
 
-window.ClickGUI.Enabled = true
-window.MainFrame.Visible = true
+print(window)
