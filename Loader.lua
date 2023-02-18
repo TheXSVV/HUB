5caa1b7a77e3410539875aa01725aa845fc5022b
TheXSVV <sowgsgd@gmail.com>
2023-02-18 23:00:42 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 function loadScript(script)
-    return game:HttpGetAsync('https://raw.githubusercontent.com/TheXSVV/HUB/main/' .. script .. '.lua')
+    return game:HttpGetAsync('https://raw.githubusercontent.com/TheXSVV/HUB/main/' .. script .. '.lua')()
 end
 
 loadScript('Checkbox')
