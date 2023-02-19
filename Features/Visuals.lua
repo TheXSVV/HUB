79ffe0a635d9cdc6ec934c2ea943ec6fd32384bd
TheXSVV <sowgsgd@gmail.com>
2023-02-19 17:57:56 +0300

    Update Visuals.lua

--- Features/Visuals.lua
+++ Features/Visuals.lua
@@ -13,6 +13,19 @@ visuals.register_me = function(window)
     local label = loadModuleScript('Settings/Label')
 
     local tab = feature:new('Visuals')
+    tab:add_setting(label:new('Chams'))
+    tab:add_setting(checkbox:new('Enable', false))
+    tab:add_setting(checkbox:new('Team check', true))
+
+    tab:add_setting(label:new('ESP'))
+    tab:add_setting(checkbox:new('Enable', false))
+    tab:add_setting(checkbox:new('Box', false))
+    tab:add_setting(checkbox:new('Nametag', false))
+    tab:add_setting(checkbox:new('Health Bar', false))
+
+    tab:add_setting(label:new('Tracers'))
+    tab:add_setting(checkbox:new('Health Bar', false))
+    tab:add_setting(checkbox:new('Health Bar', false))
 
     window.register_feature(tab)
 end
