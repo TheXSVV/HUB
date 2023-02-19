c6376533eed205b63851591d11f93cd9eacb270e
TheXSVV <sowgsgd@gmail.com>
2023-02-19 17:14:31 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,8 +2,12 @@ function loadModuleScript(script)
     return loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/TheXSVV/HUB/main/' .. script .. '.lua'))()
 end
 
+print('1')
 local utils = loadModuleScript('Utils')
+print('2')
 local window = loadModuleScript('Window')
 
+print('3')
 loadModuleScript('Features/Visuals').register_me(window)
+print('4')
 window.show()
