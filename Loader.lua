154378c94082d66e809cd86062619de070322b5c
TheXSVV <sowgsgd@gmail.com>
2023-02-19 11:27:42 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,5 +2,5 @@ function loadModuleScript(script)
     return loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/TheXSVV/HUB/main/' .. script .. '.lua'))()
 end
 
-loadModuleScript('Window').show()
 loadModuleScript('Features/TestFeature').register_me()
+loadModuleScript('Window').show()
